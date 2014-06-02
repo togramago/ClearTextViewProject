c26a93ef0ab005ad99359ffb87f51c9230cccd24
Margarita Litkevych <mlitkevych@gmail.com>
2014-06-02 22:39:58 +0300

    Text and background changes work correctly

--- src/main/java/com/cleartextviewproject/app/ClearTextView.java
+++ src/main/java/com/cleartextviewproject/app/ClearTextView.java
@@ -7,27 +7,73 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
 /**
+ * TextView extended class with transparent carved-through-background text. <br/>
+ * However, drawables from TextView are considered as part of background. <br/>
+ * <br/>
+ * Text can be seen again after call to  {@link android.widget.TextView#setTextColor(int)}
+ * as it's color is set to Color.TRANSPARENT after carving. <br/>
+ * <br/>
+ * Consider using android:freezesText="true" to save programmatically done text changes (if any).
+ * Manually save other changes (background, textSize).
+ * <br/>
+ * DO NOT OVERRIDE {@link android.widget.TextView#getBackground()}!
+ *
  * @author Margarita Litkevych
  * @version 1.0
  */
 public class ClearTextView extends TextView {
 
+    /**
+     * Saves initial background drawable
+     * in order to allow Android to care
+     * about it's size on text, background
+     * and configuration changes.
+     */
+    private Drawable mBackground;
+
+    private Paint mEraserPaint;
+
     public ClearTextView(Context context) {
         super(context);
+        setup();
+        mBackground = getBackground();
     }
 
     public ClearTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setup();
+        mBackground = getBackground();
     }
 
     public ClearTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        setup();
+        mBackground = getBackground();
+    }
+
+    private void setup() {
+        mEraserPaint = new Paint();
+        mEraserPaint.set(getPaint());
+        mEraserPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
+    }
+
+    public Paint getEraserPaint() {
+        return mEraserPaint;
+    }
+
+    public void setEraserPaint(Paint eraserPaint) {
+        if (eraserPaint != null) {
+            mEraserPaint = eraserPaint;
+        }
     }
 
     @Override
@@ -35,27 +81,120 @@ public class ClearTextView extends TextView {
         super.onSizeChanged(w, h, oldw, oldh);
 
         if (w > 0 && h > 0) {
-            Bitmap bg = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
 
-            Canvas canvas = new Canvas(bg);
-            Drawable drawable = getBackground();
-            if (drawable != null) {
-                drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-                drawable.draw(canvas);
+            if (mBackground == null) {
+                mBackground = getBackground();
+            } else {
+                setBackgroundDrawable(mBackground);
             }
 
-            Bitmap bp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-            setBackground(null);
-            setTextColor(Color.BLACK);
-            draw(new Canvas(bp));
-
-            Paint eraserPaint = new Paint();
-            eraserPaint.set(getPaint());
-            eraserPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
-            canvas.drawBitmap(bp, 0, 0, eraserPaint);
-
-            setBackground(new BitmapDrawable(getResources(), bg));
-            setTextColor(Color.TRANSPARENT);
+            if (mBackground != null) {
+                mBackground.setBounds(0, 0, w, h);
+                carveText();
+            }
         }
+    }
+
+    /**
+     * Copies text as bitmap and applies it as mask to the bitmap background.
+     * Sets result as background.
+     * Set text color to transaprent in order to let {@link android.widget.TextView#getText()} work correctly.
+     */
+    private void carveText() {
+        if (mBackground != null) {
+            Rect rect = mBackground.getBounds();
+            if (rect != null && rect.isEmpty() == false) {
+                //copy background to bitmap
+                Bitmap background = Bitmap.createBitmap(rect.width(), rect.height(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(background);
+                mBackground.draw(canvas);
+
+                //copy text to bitmap
+                Bitmap text = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
+                setBackgroundDrawable(null);
+                if (getCurrentTextColor() == Color.TRANSPARENT) {
+                    setTextColor(Color.BLACK);
+                }
+                draw(new Canvas(text));
+
+                //apply text bitmap as mask
+                if (mEraserPaint == null) {
+                    setup();
+                }
+                canvas.drawBitmap(text, 0, 0, mEraserPaint);
+
+                //set result as background drawable
+                setBackgroundDrawable(new BitmapDrawable(getResources(), background));
+
+                //set text color transparent and let getText work correctly
+                setTextColor(Color.TRANSPARENT);
+            }
+        }
+    }
+
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        if (mBackground != null) {
+            setBackgroundDrawable(mBackground);
+            carveText();
+        } else {
+            Log.e("ClearTextView", "background cannot be restored.");
+        }
+    }
+
+    /**
+     * Sets background to null. Call {@link android.widget.TextView#setTextColor(int)} to make text visible.
+     */
+    public void clearBackground() {
+        super.setBackgroundDrawable(null);
+        mBackground = null;
+    }
+
+    private void onBackgroundChanged(Drawable d) {
+        if (d == null) {
+            clearBackground();
+        } else {
+            if (d.equals(mBackground) == false) {
+                mBackground = d;
+                carveText();
+            }
+        }
+    }
+
+    @Override
+    public void setBackgroundColor(int color) {
+        super.setBackgroundColor(color);
+        onBackgroundChanged(getBackground());
+    }
+
+    @Override
+    public void setBackgroundResource(int resid) {
+        super.setBackgroundResource(resid);
+        onBackgroundChanged(getBackground());
+    }
+
+    @Override
+    public void setBackground(Drawable background) {
+        if (Build.VERSION.SDK_INT >= 16) {
+            super.setBackground(background);
+            onBackgroundChanged(getBackground());
+        } else {
+            setBackgroundDrawable(background);
+        }
+    }
+
+    @Override
+    public void setBackgroundDrawable(Drawable background) {
+        super.setBackgroundDrawable(background);
+        onBackgroundChanged(getBackground());
+    }
+
+    /**
+     * Get initial background
+     *
+     * @return background without carved-through text
+     */
+    public Drawable getFilledBackground() {
+        return mBackground;
     }
 }
--- src/main/java/com/cleartextviewproject/app/MainActivity.java
+++ src/main/java/com/cleartextviewproject/app/MainActivity.java
@@ -4,13 +4,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView tv_1_1;
+    private ClearTextView tv_1_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_1_1 = (TextView) findViewById(R.id.tv_1_1);
+        tv_1_3 = (ClearTextView) findViewById(R.id.tv_1_3);
+        tv_1_3.setText("CHANGED TEXT");
+
     }
 
 
@@ -28,8 +37,17 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            //Does not work after configuration change:
+            tv_1_3.setBackgroundDrawable(getResources().getDrawable(R.drawable.adjust_background));
+            tv_1_3.setTextSize(50);
+
+            //Works after configuration change because of android:freezesText="true"
+            tv_1_3.setText("SETTINGS LONG LONG TEXT");
+            tv_1_1.setText("just settings");
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
